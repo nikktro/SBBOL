72a45bc4c5cc0c0015f5c8526d6c1e7391753388
Nikolay Trofimov <nikktro@gmail.com>
2021-07-13 21:02:19 +0300

    add missing file TranslateModel.swift

---
+++ SBBOL/Models/TranslateModel.swift
@@ -0,0 +1,16 @@
+//
+//  TranslateModel.swift
+//  SBBOL
+//
+//  Created by Nikolay Trofimov on 01.07.2021.
+//
+
+import Foundation
+
+struct TranslateModel {
+    var source: String
+    var sourceLang: String
+    var target: String
+    var targetLang: String
+    var date: Date
+}
